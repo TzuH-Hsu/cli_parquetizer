a88f01f6874e478f03bc5bb225d8f8e96cd31aaf
TzuH-Hsu <96853116+TzuH-Hsu@users.noreply.github.com>
2024-03-18 01:39:32 +0000

    Add Progress class to _utils.py

---
+++ src/parquetizer/_utils.py
@@ -0,0 +1,18 @@
+from tqdm import tqdm
+
+
+class Progress(tqdm):
+    """A modified version of tqdm that meets the requirements of ProgressType in Minio Python SDK."""  # noqa: E501
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def set_meta(self, object_name: str, total_length: int) -> None:
+        """Set process meta information.
+
+        Args:
+            object_name (str): The name of the object.
+            total_length (int): The total length of the object.
+        """
+        self.set_description_str(f"Uploading {object_name}")
+        self.total = total_length
